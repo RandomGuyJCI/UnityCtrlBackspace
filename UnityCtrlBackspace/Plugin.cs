751677479cfe748951d583d16163c62e171a6cfc
RandomGuyJCI <carleisaguirre@gmail.com>
2022-05-22 02:03:57 +0800

    Add comments for full explanation

--- UnityCtrlBackspace/Plugin.cs
+++ UnityCtrlBackspace/Plugin.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Reflection.Emit;
 using BepInEx;
 using HarmonyLib;
@@ -17,6 +16,27 @@ namespace UnityCtrlBackspace
 
         public static class PatchInputField
         {
+            /*
+            In order to override the key behavior for pressing ctrl-backspace and prevent the first character from being deleted prematurely, 
+            we need to know when ctrl is being held in the KeyPressed() method.
+            
+            Based on the decompiled assembly, we know there is a flag4 variable in the function that determines if ctrl is being held,
+            which means we can use a transpiler to read that value to be used in patching the Backspace method later.
+            
+            This essentially edits the backspace logic in KeyPressed() from
+            
+                case KeyCode.Backspace:
+		            Backspace();
+		            return EditState.Continue;
+		            
+		    to
+		    
+		        case KeyCode.Backspace:
+		            isCtrlHeld = flag4;
+		            Backspace();
+		            return EditState.Continue;
+            */
+            
             private static bool isCtrlHeld = false;
             
             [HarmonyTranspiler]
@@ -31,6 +51,11 @@ namespace UnityCtrlBackspace
                         new CodeInstruction(OpCodes.Stsfld, AccessTools.Field(typeof(PatchInputField), nameof(isCtrlHeld))))
                     .InstructionEnumeration();
             }
+            
+            /*
+            We can now inject our own logic into the Backspace() method so that different behavior is handled for deleting text
+            if ctrl-backspace is pressed. This patch is mostly copied over from the original decompiled method.
+            */
             
             [HarmonyPrefix]
             [HarmonyPatch(typeof(InputField), "Backspace")]
@@ -61,7 +86,12 @@ namespace UnityCtrlBackspace
                 }
                 return false;
             }
-
+            
+            /*
+            The actual method that handles deleting the word. This takes advantage of a modified version of the
+            FindtPrevWordBegin method that can be found from the decompiled assembly.
+            */
+            
             private static void BackspaceWord(ref InputField __instance)
             {
                 var newCaretPosition = ModifiedFindPrevWordBegin(__instance.m_Text, __instance.caretPositionInternal);
@@ -70,7 +100,14 @@ namespace UnityCtrlBackspace
                 __instance.m_Text = __instance.text.Remove(newCaretPosition, moveLength);
                 __instance.caretPositionInternal = __instance.caretSelectPositionInternal = newCaretPosition;
             }
-
+            
+            /*
+            FindtPrevWordBegin() is used by the InputField class for moving the caret word by word when pressing ctrl-left.
+            Although we can directly use that method for BackspaceWord(), it's found to be inconsistent in actually finding the beginning of the previous word.
+            For example, pressing ctrl-left on the end of "test string..." would move the caret to the 2nd period instead of the g.
+            I therefore had to create my own modified version of that method that properly handles all edge cases.
+            */
+            
             private static int ModifiedFindPrevWordBegin(string text, int caretPosition)
             {
                 var lastCaretPosition = caretPosition - 1;
